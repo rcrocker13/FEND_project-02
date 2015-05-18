7f4946ce9b99b3c59496deae450e09f95d9f6cd5
Robert Crocker <rcrocker13@Roberts-MacBook-Pro.local>
2015-05-17 19:19:12 -0700

    Update build.js to append text to the Header.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,10 @@
-$("#main").append("Robert Crocker");
+var awesomeThoughts;
+var funThoughts;
+
+var myName = "Robert Crocker";
+
+awesomeThoughts = "I am " + myName + " and I am AWESOME!"
+
+funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
